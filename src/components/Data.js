e93ca6031e1251b90e3f557222d0f50681b70699
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2023-08-18 05:17:59 +0500

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,47 @@
+const Data = {
+  productItems: [
+    {
+      id: 1,
+      discount: 50,
+      cover: "./images/kitchen_acc/img10.png",
+      name: "tackable Refrigerator Organizer",
+      price: 100,
+    },
+    {
+      id: 2,
+      discount: 40,
+      cover: "./images/kitchen_acc/img11.png",
+      name: "Manual Food Chopper",
+      price: 20,
+    },
+    {
+      id: 3,
+      discount: 40,
+      cover: "./images/kitchen_acc/img12.png",
+      name: "Electric Garlic Chopper,",
+      price: 200,
+    },
+    {
+      id: 4,
+      discount: 40,
+      cover: "./images/kitchen_acc/img13.png",
+      name: "Strainer Basket",
+      price: 50,
+    },
+    {
+      id: 5,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 100,
+    },
+    {
+      id: 6,
+      discount: 50,
+      cover: "./images/flash/flash-3.png",
+      name: "Shoes",
+      price: 100,
+    },
+  ],
+}
+export default Data
